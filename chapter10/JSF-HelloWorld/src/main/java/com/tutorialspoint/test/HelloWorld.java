a1330d06057af1dbc9f7f03de9607d1c771b82e7
Vladimir Atoyan <vladimir.atoyan.external@atos.net>
2019-04-23 15:12:20 +0400

    Changed formattings.

---
+++ chapter10/JSF-HelloWorld/src/main/java/com/tutorialspoint/test/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.tutorialspoint.test;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "helloWorld", eager = true)
+public class HelloWorld {
+
+    public HelloWorld() {
+        System.out.println("HelloWorld started!");
+    }
+
+    public String getMessage() {
+        return "Hello World!";
+    }
+}
